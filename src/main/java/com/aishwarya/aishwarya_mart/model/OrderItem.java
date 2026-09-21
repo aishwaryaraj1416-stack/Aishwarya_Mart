1e4641165e1e60c50b3a27e66b438d4bd176310a
Aishwarya Raj <aishwaryaraj1416@gmail.com>
2026-09-21 07:10:13 +0530

    model: add order item entity

---
+++ src/main/java/com/aishwarya/aishwarya_mart/model/OrderItem.java
@@ -0,0 +1,90 @@
+package com.aishwarya.aishwarya_mart.model;
+
+import java.math.BigDecimal;
+
+public class OrderItem {
+
+    private int id;
+    private int orderId;
+    private int productId;
+    private int quantity;
+    private BigDecimal price;
+    private String productName;
+    private String imageUrl;
+
+    public OrderItem() {
+    }
+
+    public OrderItem(int id, int orderId, int productId,
+                     int quantity, BigDecimal price) {
+        this.id = id;
+        this.orderId = orderId;
+        this.productId = productId;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public OrderItem(int orderId, int productId,
+                     int quantity, BigDecimal price) {
+        this.orderId = orderId;
+        this.productId = productId;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
